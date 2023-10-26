da77c3adea8d7442706d1c5f791ad50177584ef5
2258250052 <113990029+2258250052@users.noreply.github.com>
2023-10-26 22:10:52 +0530

    Create largest.py

---
+++ largest.py
@@ -0,0 +1,11 @@
+num1 = float(input())
+num2 = float(input())
+num2 = float(input())
+
+if (num1 >= num2) and (num1  = num3):
+  largest =  um1
+elif (num2 > num1) and (num2 > = num3):
+  largest = num2
+else:
+  largest = num3
+  print("largest")

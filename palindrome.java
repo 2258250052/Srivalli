2c93859316c94e98773a238337d9d678212dc559
2258250052 <113990029+2258250052@users.noreply.github.com>
2023-10-26 22:02:20 +0530

    Create palindrome.py

---
+++ palindrome.java
@@ -0,0 +1,12 @@
+num=int(input("Enter a number:"))
+  temp=num
+  rev=0
+  while(num>o):
+dig=num%10
+  rev=rev*10+dig
+  num=num//10
+  if(temp==rev):
+print("the number is palindrome")
+  else:
+print("not palindrome")
+  
